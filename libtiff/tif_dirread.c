f1826525bc2eaa46566d3484be670ee82017f23d
Frank Warmerdam <warmerdam@pobox.com>
2005-05-22 17:36:50 +0000

    avoid bogus stripbytecount messages if stripoffset zero

--- libtiff/tif_dirread.c
+++ libtiff/tif_dirread.c
@@ -1,4 +1,4 @@
-/* $Id: tif_dirread.c,v 1.53 2005-05-06 14:35:50 fwarmerdam Exp $ */
+/* $Id: tif_dirread.c,v 1.54 2005-05-22 17:36:50 fwarmerdam Exp $ */
 
 /*
  * Copyright (c) 1988-1997 Sam Leffler
@@ -591,7 +591,10 @@ TIFFReadDirectory(TIFF* tif)
       (tif->tif_mode == O_RDONLY && \
        td->td_compression == COMPRESSION_NONE && \
        td->td_stripbytecount[0] < TIFFScanlineSize(tif) * td->td_imagelength) )
-	} else if (td->td_nstrips == 1 && BYTECOUNTLOOKSBAD) {
+
+	} else if (td->td_nstrips == 1 
+                   && td->td_stripoffset[0] != 0 
+                   && BYTECOUNTLOOKSBAD) {
 		/*
 		 * Plexus (and others) sometimes give a value
 		 * of zero for a tag when they don't know what
