0c1c40567820752928d953db06b392d097000e2a
Andrey Kiselev <dron@ak4719.spb.edu>
2005-07-28 14:49:37 +0000

    Small fix for the previous commit.

--- libtiff/tiffiop.h
+++ libtiff/tiffiop.h
@@ -1,4 +1,4 @@
-/* $Id: tiffiop.h,v 1.39 2005-07-28 11:35:12 dron Exp $ */
+/* $Id: tiffiop.h,v 1.40 2005-07-28 14:49:37 dron Exp $ */
 
 /*
  * Copyright (c) 1988-1997 Sam Leffler
@@ -268,7 +268,7 @@ extern	void _TIFFprintAsciiTag(FILE*, const char*, const char*);
 GLOBALDATA(TIFFErrorHandler,_TIFFwarningHandler);
 GLOBALDATA(TIFFErrorHandler,_TIFFerrorHandler);
 
-extern	char *_TIFFCheckMalloc(TIFF*, size_t, size_t, const char*);
+extern	tdata_t _TIFFCheckMalloc(TIFF*, size_t, size_t, const char*);
 
 extern	int TIFFInitDumpMode(TIFF*, int);
 #ifdef PACKBITS_SUPPORT
