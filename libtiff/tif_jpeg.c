bb8117904cec66f10d090cb5d0db28c0fc864c2a
Frank Warmerdam <warmerdam@pobox.com>
2008-12-21 21:07:05 +0000

    minor formatting changes

--- libtiff/tif_jpeg.c
+++ libtiff/tif_jpeg.c
@@ -1,4 +1,4 @@
-/* $Id: tif_jpeg.c,v 1.78 2008-12-21 20:13:38 fwarmerdam Exp $ */
+/* $Id: tif_jpeg.c,v 1.79 2008-12-21 21:07:05 fwarmerdam Exp $ */
 
 /*
  * Copyright (c) 1994-1997 Sam Leffler
@@ -1721,10 +1721,11 @@ JPEGEncode(TIFF* tif, uint8* buf, tmsize_t cc, uint16 s)
 	/* data is expected to be supplied in multiples of a scanline */
 	nrows = cc / sp->bytesperline;
 	if (cc % sp->bytesperline)
-		TIFFWarningExt(tif->tif_clientdata, tif->tif_name, "fractional scanline discarded");
+            TIFFWarningExt(tif->tif_clientdata, tif->tif_name, 
+                           "fractional scanline discarded");
 
         /* The last strip will be limited to image size */
-        if( !isTiled(tif) && tif->tif_row + nrows > tif->tif_dir.td_imagelength )
+        if( !isTiled(tif) && tif->tif_row+nrows > tif->tif_dir.td_imagelength )
             nrows = tif->tif_dir.td_imagelength - tif->tif_row;
 
 	while (nrows-- > 0) {
