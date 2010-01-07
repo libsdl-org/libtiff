f132add071bc50223a4b0b31fd18ddce53eeae1f
Frank Warmerdam <warmerdam@pobox.com>
2010-01-07 04:05:38 +0000

    ensure tile and strip sizes are reset when loading new directory (#1936)

--- libtiff/tif_dir.c
+++ libtiff/tif_dir.c
@@ -1,4 +1,4 @@
-/* $Id: tif_dir.c,v 1.75.2.2 2009-01-01 00:10:43 bfriesen Exp $ */
+/* $Id: tif_dir.c,v 1.75.2.3 2010-01-07 04:05:39 fwarmerdam Exp $ */
 
 /*
  * Copyright (c) 1988-1997 Sam Leffler
@@ -1099,6 +1099,11 @@ TIFFDefaultDirectory(TIFF* tif)
 	 * Should we also be clearing stuff like INSUBIFD?
 	 */
 	tif->tif_flags &= ~TIFF_ISTILED;
+        /*
+         * Clear other directory-specific fields.
+         */
+        tif->tif_tilesize = -1;
+        tif->tif_scanlinesize = -1;
 
 	return (1);
 }
