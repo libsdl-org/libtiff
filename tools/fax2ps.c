467aa4ebdc95e66441db43b485919653cc82ad9c
Lee Howard <faxguy@howardsilvan.com>
2010-12-12 01:12:29 +0000

    * tools/fax2ps.c: replace unsafe tmpfile() with mkstemp()         http://bugzilla.maptools.org/show_bug.cgi?id=2118

--- tools/fax2ps.c
+++ tools/fax2ps.c
@@ -1,4 +1,4 @@
-/* $Id: fax2ps.c,v 1.22.2.1 2010-06-08 18:50:43 bfriesen Exp $" */
+/* $Id: fax2ps.c,v 1.22.2.2 2010-12-12 01:12:29 faxguy Exp $" */
 
 /*
  * Copyright (c) 1991-1997 Sam Leffler
@@ -377,11 +377,12 @@ main(int argc, char** argv)
 	int n;
 	FILE* fd;
 	char buf[16*1024];
+	char temp[1024];
 
-	fd = tmpfile();
+	strcpy(temp, "/tmp/fax2psXXXXXX");
+	fd = fdopen(mkstemp(temp), "w+");
 	if (fd == NULL) {
-	    fprintf(stderr, "Could not create temporary file, exiting.\n");
-	    fclose(fd);
+	    fprintf(stderr, "Could not create temp file \"%s\"\n", temp);
 	    exit(-2);
 	}
 #if defined(HAVE_SETMODE) && defined(O_BINARY)
