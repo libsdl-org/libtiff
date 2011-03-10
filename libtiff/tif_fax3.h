7f10ecccc018ec0ca491e36e6af2502a0af07372
Frank Warmerdam <warmerdam@pobox.com>
2011-03-10 20:22:33 +0000

    allow zero length runs at start of fax3 lines

--- libtiff/tif_fax3.h
+++ libtiff/tif_fax3.h
@@ -1,4 +1,4 @@
-/* $Id: tif_fax3.h,v 1.5.2.2 2011-03-03 01:36:10 fwarmerdam Exp $ */
+/* $Id: tif_fax3.h,v 1.5.2.3 2011-03-10 20:22:33 fwarmerdam Exp $ */
 
 /*
  * Copyright (c) 1990-1997 Sam Leffler
@@ -479,8 +479,10 @@ done1d:									\
 	case S_VL:							\
 	    CHECK_b1;							\
 	    if (b1 <= (int) (a0 + TabEnt->Param)) {			\
-		unexpected("VL", a0);					\
-		goto eol2d;						\
+		if (b1 < (int) (a0 + TabEnt->Param) || pa != thisrun) {	\
+		    unexpected("VL", a0);				\
+		    goto eol2d;						\
+		}							\
 	    }								\
 	    SETVALUE(b1 - a0 - TabEnt->Param);				\
 	    b1 -= *--pb;						\
