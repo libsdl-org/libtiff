9544dbac0ba0d8cb86bd8af912fdba6f0863ceba
Dwight Kelly <dbmalloc@remotesensing.org>
2011-06-08 18:44:47 +0000

    fix for Bug 2247 - tif_apple.c _tiffSizeProc() does not pass enough arguments to TIFFErrorExt()

--- libtiff/tif_apple.c
+++ libtiff/tif_apple.c
@@ -1,4 +1,4 @@
-/* $Header: /cvs/maptools/cvsroot/libtiff/libtiff/Attic/tif_apple.c,v 1.3.2.1 2010-06-08 18:50:41 bfriesen Exp $ */
+/* $Header: /cvs/maptools/cvsroot/libtiff/libtiff/Attic/tif_apple.c,v 1.3.2.2 2011-06-08 18:44:47 dbmalloc Exp $ */
 
 /*
  * Copyright (c) 1988-1997 Sam Leffler
@@ -125,7 +125,7 @@ _tiffSizeProc(thandle_t fd)
 	long size;
 
 	if (GetEOF((short) fd, &size) != noErr) {
-		TIFFErrorExt(fd, "_tiffSizeProc", "%s: Cannot get file size");
+		TIFFErrorExt(fd, "_tiffSizeProc", "Cannot get file size");
 		return (-1L);
 	}
 	return ((toff_t) size);
