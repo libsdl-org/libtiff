709f14f258fe4a2603644b9cf26197cfd769ff76
Lee Howard <faxguy@howardsilvan.com>
2016-12-14 18:36:27 +0000

    HylaFAX not Halyfax

--- libtiff/tif_fax3.h
+++ libtiff/tif_fax3.h
@@ -1,4 +1,4 @@
-/* $Id: tif_fax3.h,v 1.12 2016-12-13 18:15:48 erouault Exp $ */
+/* $Id: tif_fax3.h,v 1.13 2016-12-14 18:36:27 faxguy Exp $ */
 
 /*
  * Copyright (c) 1990-1997 Sam Leffler
@@ -81,7 +81,7 @@ extern void _TIFFFax3fillruns(unsigned char*, uint32*, uint32*, uint32);
 #define S_MakeUp   11
 #define S_EOL      12
 
-/* WARNING: do not change the layout of this structure as the Halyfax software */
+/* WARNING: do not change the layout of this structure as the HylaFAX software */
 /* really depends on it. See http://bugzilla.maptools.org/show_bug.cgi?id=2636 */
 typedef struct {                /* state table entry */
 	unsigned char State;    /* see above */
