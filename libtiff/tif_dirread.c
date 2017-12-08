7293a4165b5c5d8ba4f18134b2201e653e193cb8
Vadim Zeitlin <vadim@zeitlins.org>
2017-12-08 03:27:29 +0100

    Remove tests for undefined SIZEOF_VOIDP
    As configure never uses AC_CHECK_SIZEOF(void*), this symbol is never
    defined and so it doesn't make sense to test it in the code, this just
    results in -Wundef warnings if they're enabled.

--- libtiff/tif_dirread.c
+++ libtiff/tif_dirread.c
@@ -774,7 +774,7 @@ static enum TIFFReadDirEntryErr TIFFReadDirEntryIfd8(TIFF* tif, TIFFDirEntry* di
 static enum TIFFReadDirEntryErr TIFFReadDirEntryDataAndRealloc(
                     TIFF* tif, uint64 offset, tmsize_t size, void** pdest)
 {
-#if SIZEOF_VOIDP == 8 || SIZEOF_SIZE_T == 8
+#if SIZEOF_SIZE_T == 8
         tmsize_t threshold = INITIAL_THRESHOLD;
 #endif
         tmsize_t already_read = 0;
@@ -795,7 +795,7 @@ static enum TIFFReadDirEntryErr TIFFReadDirEntryDataAndRealloc(
             void* new_dest;
             tmsize_t bytes_read;
             tmsize_t to_read = size - already_read;
-#if SIZEOF_VOIDP == 8 || SIZEOF_SIZE_T == 8
+#if SIZEOF_SIZE_T == 8
             if( to_read >= threshold && threshold < MAX_THRESHOLD )
             {
                 to_read = threshold;
