2480971bba0595c7f560813f2cbb1695cceb389e
Bob Friesenhahn <bfriesen@simple.dallas.tx.us>
2018-11-03 13:27:20 -0500

    tiff2pdf: Eliminate compiler warning about snprintf output truncation when formatting pdf_datetime.

--- tools/tiff2pdf.c
+++ tools/tiff2pdf.c
@@ -4251,13 +4251,13 @@ void t2p_pdf_currenttime(T2P* t2p)
 
 	currenttime = localtime(&timenow);
 	snprintf(t2p->pdf_datetime, sizeof(t2p->pdf_datetime),
-		 "D:%.4d%.2d%.2d%.2d%.2d%.2d",
-		 (currenttime->tm_year + 1900) % 65536,
-		 (currenttime->tm_mon + 1) % 256,
-		 (currenttime->tm_mday) % 256,
-		 (currenttime->tm_hour) % 256,
-		 (currenttime->tm_min) % 256,
-		 (currenttime->tm_sec) % 256);
+		 "D:%.4u%.2u%.2u%.2u%.2u%.2u",
+		 TIFFmin((unsigned) currenttime->tm_year + 1900U,9999U),
+		 TIFFmin((unsigned) currenttime->tm_mon + 1U,12U),   /* 0-11 + 1 */
+		 TIFFmin((unsigned) currenttime->tm_mday,31U),       /* 1-31 */
+		 TIFFmin((unsigned) currenttime->tm_hour,23U),       /* 0-23 */
+		 TIFFmin((unsigned) currenttime->tm_min,59U),        /* 0-59 */
+		 TIFFmin((unsigned) (currenttime->tm_sec),60U));     /* 0-60 */
 
 	return;
 }
