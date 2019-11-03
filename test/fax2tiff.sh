aeb87392ad20d22cffd2e8f0674cd581b7061cc6
Bob Friesenhahn <bfriesen@simple.dallas.tx.us>
2019-11-03 09:55:14 -0600

    Fax2tiff.sh needs to remove its output file in advance. Syntax changes so that bash is not required.

--- test/fax2tiff.sh
+++ test/fax2tiff.sh
@@ -5,8 +5,9 @@
 . ${srcdir:-.}/common.sh
 infile="${IMAGES}/miniswhite-1c-1b.g3"
 outfile="o-fax2tiff.tiff"
+rm -f $outfile
 echo "$MEMCHECK ${FAX2TIFF} -M -o $outfile $infile"
-eval "$MEMCHECK" "${FAX2TIFF}" "-M" "-o" "$outfile" "$infile"
+eval $MEMCHECK ${FAX2TIFF} -M -o $outfile $infile
 status=$?
 if [ $status != 0 ] ; then
   echo "Returned failed status $status!"
