31073933543e6f89397a5c72f5f061a57f2353f4
Thomas Bernard <miniupnp@free.fr>
2020-02-08 13:27:51 +0100

    tiff2pdf: palette bound check in t2p_sample_realize_palette()
    fixes #82

--- tools/tiff2pdf.c
+++ tools/tiff2pdf.c
@@ -3731,6 +3731,11 @@ tsize_t t2p_sample_realize_palette(T2P* t2p, unsigned char* buffer){
 	for(i=sample_count;i>0;i--){
 		palette_offset=buffer[i-1] * component_count;
 		sample_offset= (i-1) * component_count;
+		if(palette_offset + component_count > t2p->pdf_palettesize){
+			TIFFError(TIFF2PDF_MODULE,
+				"Error: palette_offset + component_count > t2p->pdf_palettesize");
+			return 1;
+		}
 		for(j=0;j<component_count;j++){
 			buffer[sample_offset+j]=t2p->pdf_palette[palette_offset+j];
 		}
