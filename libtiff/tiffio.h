eb36ce944f4e836d9e7562848a6bf3c0b1189c92
Yuriy Chernyshov <georgthegreat@gmail.com>
2023-09-05 09:44:52 +0000

    Fix using __attribute__ libtiff with clang-for-windows

--- libtiff/tiffio.h
+++ libtiff/tiffio.h
@@ -261,7 +261,7 @@ typedef struct {
 #define LOGLUV_PUBLIC 1
 #endif
 
-#if defined(__GNUC__) || defined(__attribute__)
+#if defined(__GNUC__) || defined(__clang__) || defined(__attribute__)
 #  define TIFF_ATTRIBUTE(x)    __attribute__(x)
 #else
 #  define TIFF_ATTRIBUTE(x) /*nothing*/
