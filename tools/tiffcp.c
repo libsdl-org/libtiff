2cccf6d75bfe962d9b6e5c4b856c774a8a92a952
Even Rouault <even.rouault@spatialys.com>
2023-10-07 00:40:50 +0200

    tiffcp: replace PHOTOMETRIC_YCBCR with PHOTOMETRIC_RGB when outputing to compression != JPEG (refs #571)

--- tools/tiffcp.c
+++ tools/tiffcp.c
@@ -893,7 +893,12 @@ static int tiffcp(TIFF *in, TIFF *out)
                      samplesperpixel == 1 ? PHOTOMETRIC_LOGL
                                           : PHOTOMETRIC_LOGLUV);
     else
-        CopyTag(TIFFTAG_PHOTOMETRIC, 1, TIFF_SHORT);
+    {
+        if (input_photometric == PHOTOMETRIC_YCBCR)
+            TIFFSetField(out, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB);
+        else
+            CopyTag(TIFFTAG_PHOTOMETRIC, 1, TIFF_SHORT);
+    }
     if (fillorder != 0)
         TIFFSetField(out, TIFFTAG_FILLORDER, fillorder);
     else
