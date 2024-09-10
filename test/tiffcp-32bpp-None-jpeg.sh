94f6f7315b184de1a00ed27dec8b009389310f65
Even Rouault <even.rouault@spatialys.com>
2024-09-11 01:10:10 +0200

    Fix test dependency on tiff2rgba-32BPP that breaks 'make check -jX'

--- test/tiffcp-32bpp-None-jpeg.sh
+++ test/tiffcp-32bpp-None-jpeg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Generated file, master is Makefile.am
 . ${srcdir:-.}/common.sh
-infile="o-tiff2rgba-32bpp-None-jpeg.tiff"
+infile="$srcdir/images/32bpp-None-jpeg.tiff"
 outfile="o-tiffcp-32bpp-None-jpeg-YCbCr.tiff"
 f_test_convert "${TIFFCP} -c jpeg" $infile $outfile
 f_tiffinfo_validate $outfile
